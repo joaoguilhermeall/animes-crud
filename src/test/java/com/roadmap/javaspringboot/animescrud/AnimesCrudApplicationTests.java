d7e79207cfad332bc40c2f465a96072aff271ada
Joao Guiherme Costa <joaoguilhermeall@gmail.com>
2022-02-04 17:59:29 -0300

    Start Repository

---
+++ src/test/java/com/roadmap/javaspringboot/animescrud/AnimesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmap.javaspringboot.animescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
